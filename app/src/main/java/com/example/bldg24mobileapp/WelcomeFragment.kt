87e9c1fa231e0a1e342baf4939fd1e015f814a04
marysaray <msaray.ms@outlook.com>
2024-05-08 16:56:10 -0700

    Added OnClickListener to show programs button and implemented navigation controller

--- app/src/main/java/com/example/bldg24mobileapp/WelcomeFragment.kt
+++ app/src/main/java/com/example/bldg24mobileapp/WelcomeFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.findNavController
 
 class WelcomeFragment : Fragment() {
     override fun onCreateView(
@@ -12,6 +14,14 @@ class WelcomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_welcome, container, false)
+        val view = inflater.inflate(R.layout.fragment_welcome,
+            container, false)
+        val showProgramsButton = view.findViewById<Button>(R.id.buttonShowProgramId)
+        // code that runs when the button is clicked
+        showProgramsButton.setOnClickListener {
+            view.findNavController()
+                .navigate(R.id.action_welcomeFragment_to_programsFragment)
+        }
+        return view
     }
 }
